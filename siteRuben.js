7b6e22995df8bbbb7343b21449f39e9388cca371
rubenbrami <45846602+rubenbrami@users.noreply.github.com>
2020-06-29 13:16:45 +0200

    Add files via upload

--- siteRuben.js
+++ siteRuben.js
@@ -14,13 +14,11 @@ var stopVideo = function ( element ) {
     }
 };
 
-
 var buttonAddMovie = document.getElementById("buttonAddMovie");
 buttonAddMovie.onclick = function(){
 	var v = document.createElement("FIGURE");
 	document.getElementById("CourtsMetrages").appendChild(v);
 }
-
 
 // 22 H 22
 var buton22h22 = document.getElementById("miniature_22h22");

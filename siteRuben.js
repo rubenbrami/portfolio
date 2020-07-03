6088459b449d4274aabf1b3113c6252cf3d3c6d5
rubenbrami <45846602+rubenbrami@users.noreply.github.com>
2020-07-03 13:56:43 +0200

    Update siteRuben.js

--- siteRuben.js
+++ siteRuben.js
@@ -15,6 +15,7 @@ function myFunction(){
 	}
 }
 
+
 var stopVideo = function ( element ) {
     var iframe = element.querySelector( 'iframe');
     var video = element.querySelector( 'video' );
@@ -27,13 +28,20 @@ var stopVideo = function ( element ) {
     }
 };
 
-//var croix = document.getElementsByClassName("croix");
-
 var titles = ["22h22", "LesGarsQuiCherchent", "Vermillon", "Happiness47", "Mindie", "MarionValentineTomWalker", "OpenSpace", "BlueInk", "LosingFaith", "LegoStarWars", "WallE", "Lego3D", "Charlotte"]; 
 
 var open = function (title) {
 	var fen = document.getElementById("fenetre".concat(title));
+	
 	fen.style.display= "block";
+
+	// Les quatre prochaines lignes pour que le scroll se réinitialise à chaque ouverture de fenêtre.
+
+	var zoneText = fen.childNodes[3].childNodes[1];
+	var zoneASavoir = fen.childNodes[5].childNodes[5];
+	zoneText.scrollTo(0,0);
+	zoneASavoir.scrollTo(0,0);
+
 	stopVideo(bandedemo);
 	body.style.overflow = "hidden";
 	console.log(body.style.overflow );

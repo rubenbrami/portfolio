394c303da5e9b2490107ce13d80b03bb909d19e7
rubenbrami <45846602+rubenbrami@users.noreply.github.com>
2020-07-12 20:41:51 +0200

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -1,7 +1,8 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-//header("Content-Type: text/plain"); // Utilisation d'un header pour spécifier le type de contenu de la page. Ici, il s'agit juste de texte brut (text/plain). 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header("Content-Type: text/plain"); // Utilisation d'un header pour spécifier le type de contenu de la page. Ici, il s'agit juste de texte brut (text/plain). 
 
 // try{
 	$bdd = new PDO('mysql:host=sql7.freemysqlhosting.net;dbname=sql7354367;charset=utf8','sql7354367','K2pYSVKNcs');
